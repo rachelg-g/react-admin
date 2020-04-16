c161afd01f0f4084f2846f9a0d85d1f88cf6bae3
hyung <hj.gwon@specupad.com>
2020-04-16 12:33:48 +0900

    Add UserList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Admin, Resource, ListGuesser } from 'react-admin'
+import { Admin, Resource } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
+import UserList from './User'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 const App = () => {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="users" list={ListGuesser} />
+      <Resource name="users" list={UserList} />
     </Admin>
   )
 }
