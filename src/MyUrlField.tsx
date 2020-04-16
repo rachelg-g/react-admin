35ea45ed6060d6698f030a939a516fc76c0857cf
hyung <hj.gwon@specupad.com>
2020-04-16 12:45:51 +0900

    Customizing Styles with material-ui icons

--- src/MyUrlField.tsx
+++ src/MyUrlField.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { Launch } from '@material-ui/icons'
 
+const useStyles = makeStyles({
+  link: {
+    textDecoration: 'none',
+  },
+  icon: {
+    width: '0.5em',
+    paddingLeft: 2,
+  },
+})
 const MyUrlField = ({ record = {}, source }) => {
-  return <a href={record[source]}>{record[source]}</a>
+  const classes = useStyles()
+
+  return (
+    <a href={record[source]} className={classes.link}>
+      {record[source]}
+      <Launch className={classes.icon} />
+    </a>
+  )
 }
 
 export default MyUrlField
