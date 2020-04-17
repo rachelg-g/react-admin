3b0a69a9fdf9272682f8c397323859921ab6f3bf
hyung <hj.gwon@specupad.com>
2020-04-17 15:46:07 +0900

    Adding Post Creation and Post Editing Capabilities

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Admin, Resource } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
 import UserList from './User'
-import PostList from 'Posts'
+import { PostList, PostEdit, PostCreate } from './Posts'
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
 const App = () => {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="posts" list={PostList} />
+      <Resource name="posts" list={PostList} edit={PostEdit} creat={PostCreate} />
       <Resource name="users" list={UserList} />
     </Admin>
   )
--- src/Posts.tsx
+++ src/Posts.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
-import { List, Datagrid, TextField, ReferenceField, EditButton } from 'react-admin'
+import {
+  List,
+  Datagrid,
+  TextField,
+  ReferenceField,
+  EditButton,
+  Edit,
+  SimpleForm,
+  TextInput,
+  ReferenceInput,
+  Create,
+  SelectInput,
+} from 'react-admin'
 
-const PostList = (props) => {
+export const PostList = (props) => {
   return (
     <List {...props}>
       <Datagrid>
@@ -16,4 +28,31 @@ const PostList = (props) => {
   )
 }
 
-export default PostList
+export const PostEdit = (props) => {
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <TextInput source="id" />
+        <ReferenceInput source="userId" reference="users">
+          <TextInput source="name" />
+        </ReferenceInput>
+        <TextInput source="title" />
+        <TextInput multiline source="body" />
+      </SimpleForm>
+    </Edit>
+  )
+}
+
+export const PostCreate = (props) => {
+  return (
+    <Create {...props}>
+      <SimpleForm>
+        <ReferenceInput source="userId" reference="users">
+          <SelectInput optionText="name" />
+        </ReferenceInput>
+        <TextInput source="title" />
+        <TextInput multiline source="body" />
+      </SimpleForm>
+    </Create>
+  )
+}
